acb6977e1256821d6b9ca2a64ca8d85cb8c17c30
mohammadrv20 <167023961+mohammadrv20@users.noreply.github.com>
2024-04-19 10:20:29 +0330

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,71 @@
+let mounth = 7
+
+if(mounth<=3){
+console.log("it is spring");
+}
+
+
+if(mounth >=4 && mounth <=6 ){
+    console.log("it is summer");
+}
+
+if (mounth >=7 && mounth <=9) {
+    console.log("it is fall");
+}
+
+if (mounth >=10 && mounth<=12){
+    console.log("it is winter");
+}
+
+const borj = 7
+
+
+switch(borj){
+    case "1" :
+        console.log("it is farvardin");
+        break;
+
+    case "2" :
+        console.log("it is ordibehesht");
+        break    
+    
+    case "3" :
+        console.log("it is khordar");
+        break
+
+    case "4" :
+        console.log("it is tir");
+        break
+
+    case "5" :
+        console.log("it is mordad");
+        break
+
+    case "6" :
+        console.log("it is shahrivar");
+        break
+
+    case "7" :
+        console.log("it is mehr");
+        break
+
+    case "8" :
+        console.log("it is aban");
+        break
+
+    case "9" :
+        console.log("it is azar");
+        break
+
+    case "10" :
+        console.log("it is day");
+        break
+
+    case "11" :
+        console.log("it is bahman");
+        break
+
+    case "12" :
+        console.log("it is esfand");
+        break    
+}
