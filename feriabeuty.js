be9c9c94c7d979e898907324e4140422429cd7fe
mohammadrv20 <167023961+mohammadrv20@users.noreply.github.com>
2024-06-26 10:58:09 +0330

    Add files via upload
    this simple code helps to create posts on telegram channel

---
+++ feriabeuty.js
@@ -0,0 +1,30 @@
+function telegramchan() {
+    const name = prompt("enter the name")
+    const prop1 = prompt("enter the prop")
+    const prop2 = prompt("enter the prop")
+    const prop3 = prompt("enter the prop")
+    const prop4 = prompt("enter the prop")
+    const prop5 = prompt("enter the prop")
+    const prop6 = prompt("enter the prop")
+    const prop7 = prompt("enter the prop")
+    const prop8 = prompt("enter the prop")
+    const waytuse = prompt("how use it")
+    const price = prompt("enter the price")    
+
+    console.log(`${name}😍 
+
+    ✅${prop1}
+    ✅${prop2}
+    ✅${prop3}
+    ✅${prop4}
+    ✅${prop5}
+    ✅${prop6}
+    ✅${prop7}
+    ✅${prop8}
+
+    🌹روش مصرف🌹
+    ${waytuse}
+    
+    🎉 قیمت ویژه فقط ${price}🎉`);
+}
+telegramchan()
